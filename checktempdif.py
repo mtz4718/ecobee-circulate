0ac167529e4e45365026db1fc7cf04f8857f9e8b
mtz4718 <groundswell17@gmail.com>
2019-08-21 20:22:22 -0400

    Update checktempdif.py

--- checktempdif.py
+++ checktempdif.py
@@ -1,17 +1,42 @@
-import json
+import datetime
+import logging
 import requests
-import sys
+import json
+import ast
+from datetime import datetime
+from azure.storage.blob import BlockBlobService
+
+import azure.functions as func
+
+
+def main(mytimer: func.TimerRequest) -> None:
+    utc_timestamp = datetime.datetime.utcnow().replace(
+        tzinfo=datetime.timezone.utc).isoformat()
+
+    if mytimer.past_due:
+        logging.info('The timer is past due!')
+
+    logging.info('Python timer trigger function ran at %s', utc_timestamp)
 
 
 #Define temp threshhold, number is degree x 10
 tempthresh = 40
 
+#Define storage account name
+storAcct = ''
+#Define Access Key
+storKey = ''
+
 #API PREP
 ####################################################################################
-#Grab latest Token from token.json and assign to $token
-with open('token.json') as f:
-    data = json.load(f)
+#Grab latest Token from token.json in azure blob storage and assign to $auth
+blob = BlockBlobService(storAcct, storKey)
+
+tokenblob = blob.get_blob_to_text('ecobee-token', 'token.json')
+#convert string to dict
+data = ast.literal_eval(tokenblob.content)
 auth = data['access_token']
+
 
 #API Headers
 hed = {
@@ -103,15 +128,22 @@ if fan == 1:
     while count < 3:
         responseBody['climates'][count]['coolFan'] = 'on'
         responseBody['climates'][count]['heatFan'] = 'on'
+        #Add $fanResp for logs
+        fanResp = 'Fan is turned to On'
         count += 1
 elif fan == 0:
     count = 0
     while count < 3:
         responseBody['climates'][count]['coolFan'] = 'auto'
         responseBody['climates'][count]['heatFan'] = 'auto'
+        #Add $fanResp for logs
+        fanResp = 'Fan is turned to Auto'
         count += 1
 else:
-    print("error")
+    fanresp = 'ERROR'
+responseBody['climates'][1]['heatFan']
+responseBody['climates'][2]['heatFan']
+responseBody['climates'][0]['heatFan']
 
 #ADD REQUIRED JSON KEYS FOR RESPONSE
 ####################################################################################
@@ -122,7 +154,6 @@ append["thermostat"]["program"] = responseBody
 #convert to json
 jsonReturn = json.dumps(append)
 
-
 #SEND RESPONSE
 ####################################################################################
 #assign response to variable
@@ -131,3 +162,14 @@ programResp = requests.get(apiUrl, headers=hed, params=query)
 programDict = json.loads(programResp.content)
 #Post and save output
 postResp = requests.post(apiUrl, headers=hed, data=jsonReturn)
+
+#CONFIGURE LOGGING
+####################################################################################
+#Save output and settings in prep for log, use $fanResp from previous logic statements
+postResp = str(postResp)
+date = datetime.now()
+dateTime = date.strftime("%m-%d-%Y_%H:%M:%S")
+logString = dateTime + "   " + fanResp + " :::: The API responded with " + postResp
+
+#Send log string to blob
+blob.create_blob_from_text('ecobee-log', 'log-' + dateTime + '.txt', logString)
